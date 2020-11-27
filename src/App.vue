b8a3ddab20413f072b5596031d65c72153829bad
Axone Pearce <draxone13@gmail.com>
2020-11-27 07:23:22 +0330

    add fonts add rtl config

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,45 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
-  </v-app>
+	<v-app>
+		<v-main class="blue lighten-5">
+			<v-container class="d-flex justify-center">
+				<v-col class="white rounded-lg" cols="8">
+					سلام! من به شما کمک میکنم فیلم های آلا رو با پلیر بهتری مشاهده کنین! لطفا جهت ادامه لینک آلا رو بهم بده :
+				</v-col>
+			</v-container>
+		</v-main>
+	</v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+//import HelloWorld from './components/HelloWorld';
 
 export default {
-  name: 'App',
+	name: "App",
 
-  components: {
-    HelloWorld,
-  },
+	components: {},
 
-  data: () => ({
-    //
-  }),
+	data: () => ({}),
 };
 </script>
+
+<style>
+@font-face {
+	font-family: "Iransans";
+	src: url("./assets/fonts/IRANSansWeb.woff") format("woff"),
+		url("./assets/fonts/IRANSansWeb.ttf") format("truetype");
+	font-style: normal;
+	font-weight: 400;
+}
+
+@font-face {
+	font-family: "IransansNum";
+	src: url("./assets/fonts/IRANSansWebNum.woff") format("woff"),
+		url("./assets/fonts/IRANSansWebNum.ttf") format("truetype");
+	font-style: normal;
+	font-weight: 400;
+}
+
+body , .v-application{
+	font-family: Iransans !important;
+}
+</style>
